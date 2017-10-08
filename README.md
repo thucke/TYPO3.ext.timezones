6af929deea01e6c095ad9f5f65ae2b114e3fc21d
Thomas Hucke <thucke@web.de>
2017-10-08 17:35:07 +0200

    Near initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+# TYPO3 extension "Timezones"
+
+[Documentation link](https://wiki.typo3.org/EXT/Timezones)
--- ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-$extensionClassesPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('timezones');
-$default = array(
-   'tx_timezones_pi1' => $extensionClassesPath . 'pi1/class.tx_timezones_pi1.php',
-   'tx_timezones_pi2' => $extensionClassesPath . 'pi2/class.tx_timezones_pi2.php'
-);
-return $default;
-?>
