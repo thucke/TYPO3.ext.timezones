bc7690b9be323ac2713dac2240d072590d2f316a
Thomas Hucke <15112795+thucke@users.noreply.github.com>
2017-11-18 11:23:55 +0100

    #1 Add support for TYPO3 8.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-7.0.99',
-			'typo3' => '6.2.15-7.6.99',
+			'typo3' => '6.2.15-8.7.99',
 		),
 		'conflicts' => array(
 		),
