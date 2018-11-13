96b2a9cc86dde62e4c8c188195a56cf8a1278194
Thomas Hucke <15112795+thucke@users.noreply.github.com>
2018-11-13 20:25:42 +0100

    Raise compatibility to PHP 7.2 #5

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.5.0-7.1.99',
+			'php' => '5.5.0-7.2.99',
 			'typo3' => '6.2.15-8.7.99',
 		),
 		'conflicts' => array(
